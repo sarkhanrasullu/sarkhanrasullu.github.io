1e28b95834c84612b813304e2f2a137a0c306c79
Sarkhan Rasullu <bsptechou@gmail.com>
2020-03-21 18:42:06 +0100

    changes

---
+++ reviews/reviews.js
@@ -0,0 +1,25 @@
+var fadeInDelay = 1500;
+var fadeOutDelay = 1500;
+var readingDelay = 6000;
+var reviews = [
+      {
+        review_owner:"Erdinç Aybatar",
+        txt:"Sayın Afgan Hocam, Photoshop'u bana sevdirdiğiniz için çok teşekkür ederim. Gerçekten eğitim verme konusunda çok başarılısınız. Sizden bir ricam daha var, bizim için web site tasarımı içinde kapsamlı bir eğitim verir misiniz? Çok Teşekkür ederim. Ekleme: Sayın Afgan hocama ne kadar soru sorduysam, sorularımın hepsine cevapladı, hatta videolu cevap bile gönderdi. Kendisini şiddetle tavsiye ederim bu kursu kaçırmayın. Tekrar teşekkür ederim hocam."
+      },
+      {
+        review_owner:"Alparslan özhan",
+        txt:"Oldukça detaylı ve öğretici bir kurs emekleriniz için teşekkürler .Kurs içeriği derli toplu ,alıştırmalarla öğrenilen hususların pekiştirilmesi sağlanmış. video sürelerini boğmayacak şekilde kısa tutmanız gerçekten her eğitmenin dikkat etmesi gereken bir husus. bunun için ayriyeten teşekkürler."
+    },
+    {
+        review_owner: "Murat Osmanağaoğlu",
+        txt: "Afgan beyi youtobe kanalından zaten takip ediyorum. Ben bir fotoğrafçı değilim, web tasarımları yapıyorum, youtobe kanalındaki eğitimleri bana yetiyor ve artıyor, ancak bu kurs bana çok daha fazlasını kattı. Ellerine ve ağzına sağlık."
+    },
+    {
+        review_owner: "Şamil Yeşil",
+        txt: "Fotoğraf çekmeyi çok seven bir insanım ama makinem olmadığı için telefonla çekmek zorunda kalıyorum. Etrafımdaki insanlar bana hep kendini dahada geliştir sende yetenek var derlerdi. Bir gün youtube da afgan abiye denk geldim ve Ps öğrenmeye karar verdim. Afgan abinin çok güzel bi konuşma biçimi var ve 12 yıllık eğitim hayatımda bana bu tarz bilgi öğretmeye çalışan öğretmenle karşılaşamadım. Ps ile kısıtlı kalmak istemedim Lr dersini de satın aldım. Güzel bir fotoğraf makinesi alıp kendimi dahada geliştirmeye karar verdim. Afgan abimi burada sayfalarca anlatsam az kalır. Herkesin tanıması gereken bir insansın ve benim en iyi öğretmenimsin. Umarım hak ettiğin yerlere gelirsin abim. Kendine çok iyi bak."
+    },
+    {
+        review_owner: "Murat Yıldiz",
+        txt: "dersler cok zevkli , programın yarısını gectim ve en cok öğrenmek istediğim panellerdeki secim araclarını daha etkili kullanmakti, hocamız cok sade ve anlasılır bir bizimde anlatıyor, kaynak dosyaları ile beraber hocamızla senkronize bir bicimde calısmaktayım. Saygılarımla"
+    }
+]
